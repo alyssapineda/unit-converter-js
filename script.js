1784c3404c1d001bbd41f3388446ad343e05b15b
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-11-20 20:40:15 +1300

    creating UI and js foundation

---
+++ script.js
@@ -0,0 +1,19 @@
+//Generate all conversions when click convert
+//Round numbers to 3 decimal places 
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+// INITIALISE VARIABLES
+let convertInput = document.getElementById("convert-input")
+const convertBtn = document.getElementById("convert-btn")
+let lengthConversion = document.getElementById("length-conversion")
+let volumeConversion = document.getElementById("volume-conversion")
+let massConversion = document.getElementById("mass-conversion")
+
+convertBtn.addEventListener("click", function() {
+  
+})
